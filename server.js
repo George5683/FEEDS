357a687db81a18fa1e34e8ce15727d05a19c753d
George Salomon <newtech20111@hotmail.com>
2024-10-13 16:07:37 -0400

    adding features

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ function setupServer() {
 }
 
 // Function to add a user
-function add_user(username, password, email) {
+function add_user(username, password, name, zip_code, email) {
   // Create a connection to the database
   const connection = mysql.createConnection({
     host: '192.168.0.11', // Replace with your Raspberry Pi's IP
-    user: 'admin',              // The user you created
-    password: 'password',     // The user's password
-    database: 'USERS'             // The database name
+    user: 'admin',         // The user you created
+    password: 'password',  // The user's password
+    database: 'USERS'      // The database name where USER_INFO is located
   });
 
   // Connect to the database
@@ -32,11 +32,11 @@ function add_user(username, password, email) {
       console.error('Error connecting to the database:', err);
       return;
     }
-    console.log('Connected to the database!');
+    console.log('Connected to the USERS database!');
 
     // SQL query to insert a new user
-    const sql = 'INSERT INTO logins (Username, Password, Email) VALUES (?, ?, ?)';
-    const values = [username, password, email];
+    const sql = 'INSERT INTO USER_INFO (USERNAME, PASSWORD, NAME, ZIP_CODE, EMAIL) VALUES (?, ?, ?, ?, ?)';
+    const values = [username, password, name, zip_code, email];
 
     // Execute the query
     connection.query(sql, values, (err, results) => {
@@ -62,7 +62,55 @@ function add_user(username, password, email) {
 
 //TODO: Function to update user information
 
-//TODO: Function to Insert Pantries into Pantry Table
+
+//TODO: Function to Insert Pantries into PANTRY_INFO Table
+function InsertNewPantry(username, password, name, zip_code) {
+  // Create a connection to the database
+  const connection = mysql.createConnection({
+    host: '192.168.0.11', // Replace with your Raspberry Pi's IP
+    user: 'admin',         // The user you created
+    password: 'password',  // The user's password
+    database: 'PANTRIES'   // The database name where pantry info is stored
+  });
+
+  // Connect to the database
+  connection.connect(err => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
+    }
+    console.log('Connected to the PANTRIES database!');
+
+    // SQL query to insert a new pantry
+    const sql = 'INSERT INTO PANTRY_INFO (USERNAME, PASSWORD, NAME, ZIP_CODE) VALUES (?, ?, ?, ?)';
+    const values = [username, password, name, zip_code];
+
+    // Execute the query
+    connection.query(sql, values, (err, results) => {
+      if (err) {
+        console.error('Error adding new pantry:', err);
+        return;
+      }
+      console.log('Pantry added with ID:', results.insertId);
+      
+      // Close the connection
+      connection.end(err => {
+        if (err) {
+          console.error('Error closing the connection:', err);
+        } else {
+          console.log('Connection closed.');
+        }
+      });
+    });
+  });
+}
+
+// Example usage
+// InsertNewPantry('admin', 'password', 'Admin Pantry', '12345');
+
+//TODO: Function to Create a new Table for a new Pantry within PANTRY
+
+//TODO: 
 
 // ______________________Functions Above__________________________________________________________________________
 
