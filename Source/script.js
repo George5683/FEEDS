e5b5038c69155ded8dbdbfcc510389965d273664
Zach <zachary.fisher@ufl.edu>
2024-10-22 18:00:16 -0400

    Created User Dashboard

--- Source/script.js
+++ Source/script.js
@@ -6,67 +6,87 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // Template link
-const link1 = document.getElementById('page1');
-link1.addEventListener("click", function(){
-   link1.style.color = "red";
+const infoButton1 = document.getElementById('pantry1I');
+const closeButton1 = document.getElementById('close1');
+const pan1 = document.getElementById('p1');
+const info1 = document.getElementById('i1');
+
+const infoButton2 = document.getElementById('pantry2I');
+const closeButton2 = document.getElementById('close2');
+const pan2 = document.getElementById('p2');
+const info2 = document.getElementById('i2');
+
+const infoButton3 = document.getElementById('pantry3I');
+const closeButton3 = document.getElementById('close3');
+const pan3 = document.getElementById('p3');
+const info3 = document.getElementById('i3');
+
+const infoButton4 = document.getElementById('pantry4I');
+const closeButton4 = document.getElementById('close4');
+const pan4 = document.getElementById('p4');
+const info4 = document.getElementById('i4');
+
+const infoButton5 = document.getElementById('pantry5I');
+const closeButton5 = document.getElementById('close5');
+const pan5 = document.getElementById('p5');
+const info5 = document.getElementById('i5');
+
+const infoButton6 = document.getElementById('pantry6I');
+const closeButton6 = document.getElementById('close6');
+const pan6 = document.getElementById('p6');
+const info6 = document.getElementById('i6');
+
+
+infoButton1.addEventListener("click", function(){
+    pan1.style.display = "none";
+    info1.style.display = "unset";
+})
+closeButton1.addEventListener("click", function(){
+  pan1.style.display = "unset";
+  info1.style.display = "none";
 })
 
-// Template link
-const link2 = document.getElementById('page2');
-link2.addEventListener("click", function(){
-   link2.style.color = "green";
+infoButton2.addEventListener("click", function(){
+  pan2.style.display = "none";
+  info2.style.display = "unset";
+})
+closeButton2.addEventListener("click", function(){
+pan2.style.display = "unset";
+info2.style.display = "none";
 })
 
-// Will log out the user and return them to the sign in screen
-const logOut = document.getElementById('sOut');
-logOut.addEventListener("click", function(){
-   logOut.style.color = "blue";
+infoButton3.addEventListener("click", function(){
+  pan3.style.display = "none";
+  info3.style.display = "unset";
+})
+closeButton3.addEventListener("click", function(){
+pan3.style.display = "unset";
+info3.style.display = "none";
 })
 
-// Template table
-// Made up data
-const item1 = {
-    name: "Lucky Charms",
-    stock: "5",
-    description: "They are magically delicious"
+infoButton4.addEventListener("click", function(){
+  pan4.style.display = "none";
+  info4.style.display = "unset";
+})
+closeButton4.addEventListener("click", function(){
+pan4.style.display = "unset";
+info4.style.display = "none";
+})
 
-}
-const item2 = {
-    name: "Frosted Flakes",
-    stock: "7",
-    description: "They're grrrreat"
+infoButton5.addEventListener("click", function(){
+  pan4.style.display = "none";
+  info4.style.display = "unset";
+})
+closeButton5.addEventListener("click", function(){
+pan4.style.display = "unset";
+info4.style.display = "none";
+})
 
-}
-const item3 = {
-    name: "Coco Puffs",
-    stock: "2",
-    description: "I'm coo-coo for Coco Puffs"
-
-}
-const item4 = {
-    name: "Trix",
-    stock: "3",
-    description: "Silly Rabbit, Trix are for kids"
-
-}
-
-// List of made up data
-const tempList = [item1, item2, item3, item4];
-
-
-// Gets values from made up list that will be replaced by data from the database later on
-let table = document.getElementById("items");
-for (let i = 0; i < tempList.length; i++) {
-    let newRow = table.insertRow(table.rows.length);
-    if (i/2 % 1) {
-        newRow.style.background = "rgb(237 238 242)";
-    }
-    else {
-        newRow.style.background = "white";
-    }
-    let currentItem = tempList[i];
-    newRow.insertCell(0).innerHTML = currentItem.name;
-    newRow.insertCell(1).innerHTML = currentItem.stock;
-    newRow.insertCell(2).innerHTML = currentItem.description;
-}
+infoButton6.addEventListener("click", function(){
+  pan6.style.display = "none";
+  info6.style.display = "unset";
+})
+closeButton6.addEventListener("click", function(){
+pan6.style.display = "unset";
+info6.style.display = "none";
+})
