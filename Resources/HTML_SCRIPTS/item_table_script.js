7875031c989e7b739c9c71a611bf44545de5caaa
Zach <zachary.fisher@ufl.edu>
2024-11-05 01:44:24 -0500

    Now items are shown dynamically in item browser

--- Resources/HTML_SCRIPTS/item_table_script.js
+++ Resources/HTML_SCRIPTS/item_table_script.js
@@ -15,6 +15,50 @@ async function main(){
   } else {
       console.error('Pantry name not found in the URL.');
   }
+
+  try {
+    const response = await fetch('/GetPantryItems', {
+        method: 'POST',
+        body: JSON.stringify({ pantryName: pantryName }),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    const responseData = await response.json();
+
+    if (response.ok) {
+      for (let i = 0; i < responseData.length; i++) {
+        addRow(responseData[i].FOOD_NAME, responseData[i].STATUS);
+      }
+    }
+
+  } catch (error) {
+  console.error('Error:', error);
+  alert('Error retrieving pantry information from server.');
+  }
+}
+
+function addRow(name, status) {
+  let iName = name.split(' ').join('-');
+  let bTable = document.getElementById("browserTable");
+  let newRow = bTable.insertRow(-1);
+  let newItem = newRow.insertCell(0);
+  let newStock = newRow.insertCell(1);
+  let newDate = newRow.insertCell(2);
+  let newImage = document.createElement('img');
+  newImage.src = `./Images/${iName}.png`;
+  newImage.classList.add("foodimage");
+  let nameText = document.createTextNode(name);
+  let stockText = document.createTextNode(status);
+  let dateText = document.createTextNode("11/02/2024");
+  newItem.appendChild(newImage);
+  newItem.appendChild(nameText);
+  newStock.appendChild(stockText);
+  newDate.appendChild(dateText);
+  newRow.addEventListener("click", function() {
+    showPopup(name, status, "11/02/2024");
+  });
 }
 
 function showPopup(itemName, itemStock, itemDate) {
